503a9b64ff42a8522b0fd14df9bfd433f049d9f4
Daniel Stepanenko <danielstepanenko@gmail.com>
2021-11-03 00:38:35 +1100

    Implement CAP test in controller

---
+++ CAP.API/Controllers/CapTestController.cs
@@ -0,0 +1,44 @@
+﻿using CAP.Application;
+using DotNetCore.CAP;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAP.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CapTestController : ControllerBase
+    {
+        private readonly OutboxDbContext _context;
+        private readonly ICapPublisher _capBus;
+
+        public CapTestController(OutboxDbContext context, ICapPublisher capPublisher)
+        {
+            _context = context;
+            _capBus = capPublisher;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            using (_context.Database.BeginTransaction(_capBus, autoCommit: true))
+            {
+                _context.BusinessRecords.Add(new BusinessRecord() { Name = "ef.transaction" });
+
+                _capBus.Publish("test-topic", "test-message");
+            }
+        }
+
+        [NonAction]
+        [CapSubscribe("test-topic")]
+        public void Subscriber(string message)
+        {
+            Console.WriteLine($@"{DateTime.Now} Subscriber invoked, Info: {message}");
+        }
+
+    }
+}
--- CAP.API/Startup.cs
+++ CAP.API/Startup.cs
@@ -1,10 +1,12 @@
 using CAP.Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,8 +23,11 @@ namespace CAP.API
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
             var connectionString = Configuration.GetValue<string>("DbConnection");
             var serviceBusConnection = Configuration.GetValue<string>("ServiceBusConnection");
 
@@ -44,12 +49,15 @@ namespace CAP.API
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
 
             app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
